d344323221242d3ac551d22ad8bfc2cc26d599e1
houshiyang <bithoushiyang@163.com>
2020-08-21 17:57:54 +0800

    add API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,16 @@
 //index.js
-var app = getApp(); //调用app.js中内容
+
 
 Page({
     data: {
         winHeight: "", //窗口高度
         currentTab: 0, //预设当前项的值
         scrollLeft: 0, //tab标题的滚动条位置
+        newsList: [],
+        newsType: '',
     },
     // 滚动切换标签样式
     switchTab: function (e) { //对应bindchange组件，用console.log打印e看结果结构
-        console.log(e)
         this.setData({
             currentTab: e.detail.current //取到当前页
         });
@@ -17,7 +18,6 @@ Page({
     },
     // 点击标题切换当前页时改变样式
     switchNav: function (e) { //对应bindtap组件
-        console.log(e)
         let cur = e.target.dataset.current; //取到点击的目标值
         if (this.data.currentTaB == cur) {
             return false;
@@ -53,7 +53,17 @@ Page({
                     winHeight: calc
                 });
             }
-        });
-    },
+        })
+        wx.request({
+            url: 'https://v.juhe.cn/toutiao/index?type=top&key=d82f7b5df2af715a2178e5ba8266cc73',
+            success: res => {
+                console.log(res);
+                this.setData({
+                    newsList: res.data.result.data
+                })
+            }
+        })
+    }
     //footerTap:app.footerTap
+
 })
