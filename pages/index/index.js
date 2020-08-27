ec0656d39b76d014596efae8e39f473c8fbfe5c6
houshiyang <bithoushiyang@163.com>
2020-08-27 17:20:11 +0800

    rolling completed

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
         currentTab: 0, //预设当前项的值
         scrollLeft: 0, //tab标题的滚动条位置
         newsList: [],
-        newsType: '',
+        newsType: ''
     },
     // 滚动切换标签样式
     switchTab: function (e) { //对应bindchange组件，用console.log打印e看结果结构
@@ -19,6 +19,9 @@ Page({
     // 点击标题切换当前页时改变样式
     switchNav: function (e) { //对应bindtap组件
         let cur = e.target.dataset.current; //取到点击的目标值
+        this.setData({
+            id: e.target.id
+        });
         if (this.data.currentTaB == cur) {
             return false;
         } else {
@@ -26,6 +29,7 @@ Page({
                 currentTab: cur //切换到目标页面
             })
         }
+        this.loadNews(this.data.id)
     },
     //判断当前横向滚动超过一屏时，设置tab标题滚动条。
     checkCor: function () {
@@ -48,22 +52,22 @@ Page({
                     clientWidth = res.windowWidth,
                     rpxR = 750 / clientWidth;
                 let calc = clientHeight * rpxR - 180;
-                console.log(calc)
                 that.setData({
                     winHeight: calc
                 });
             }
         })
+        this.loadNews(this.data.id)
+    },
+    //footerTap:app.footerTap
+    loadNews: function (id = 'top') {
         wx.request({
-            url: 'https://v.juhe.cn/toutiao/index?type=top&key=d82f7b5df2af715a2178e5ba8266cc73',
+            url: 'https://v.juhe.cn/toutiao/index?type=' + id + '&key=d82f7b5df2af715a2178e5ba8266cc73',
             success: res => {
-                console.log(res);
                 this.setData({
                     newsList: res.data.result.data
                 })
             }
         })
-    }
-    //footerTap:app.footerTap
-
+    },
 })
